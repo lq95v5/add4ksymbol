0956357f390bdba9322c742079e23b67e29fb3fc
qiang <lq95v5@gmail.com>
2022-07-17 10:43:18 +0800

    first commit

---
+++ add-4klogo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for image in {*.jpg,*.png}
+do
+	if [ ${image##*-} == "logo.jpg" ] || [ $image == "4k-logo.png" ] || [ -e ${image%%.*}-logo.jpg ]
+	then
+		continue
+	fi
+
+	convert $image  4k-logo.png -geometry +3350+60  -composite ${image%%.*}-logo.jpg
+done
